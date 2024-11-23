f25cf2f06fad8e6ad5e28b1b9bc3981183bc3a20
ahmedtohamy1 <1ahmed.tohamy@gmail.com>
2024-11-23 05:54:43 +0200

    aaaaaaa

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2473440779"
+    serviceWorkerVersion: "1196979002"
   }
 });
